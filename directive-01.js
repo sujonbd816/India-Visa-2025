58e75b591fef0066c2be4aa7a8922cdf9acb9fbd
WordPress & YouTube SEO Specialist <141910767+sujonbd816@users.noreply.github.com>
2025-03-01 15:15:28 +0600

    Add files via upload

---
+++ directive-01.js
@@ -0,0 +1,32 @@
+app.directive('stopccp', function(){
+    return {
+        scope: {},
+        link:function(scope,element){
+            element.on('cut copy paste', function (event) {
+            });
+        }
+    };
+});
+app.directive('tooltip', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs){
+            $(element).hover(function(){
+                // on mouseenter
+                $(element).tooltip('show');
+            }, function(){
+                // on mouseleave
+                $(element).tooltip('hide');
+            });
+        }
+    };
+});
+app.directive("modalToggle",function(){
+    return function(scope, element, attrs){
+        scope.$watch(function(){ return scope.open_modal; }, function(value) {
+            console.log(scope.open_modal);
+            if (value) element.addClass('modal-hide');
+            else element.removeClass('modal-hide');
+        });
+    }
+});
